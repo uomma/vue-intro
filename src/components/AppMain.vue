21cbba34bc52524134600d91f72f4eb70cfa5432
uomma <marcelloleone975@gmail.com>
2023-12-22 16:05:07 +0100

    mixin

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,22 +1,25 @@
 <script>
 import AppMainMenu from './AppMainMenu.vue';
+import AppMainContainer from './AppMainContainer.vue';
 export default {
     name: 'AppMain',
     components: {
         AppMainMenu,
+        AppMainContainer
     }
 
 } 
 </script>
 <template><main>
     <AppMainMenu></AppMainMenu>
+    <AppMainContainer></AppMainContainer>
 
 </main>
 </template>
 <style lang="scss" scoped>
 main{
     height: 300px;
-    background-color: black;
+   
 }
 
 </style>
---
+++ src/components/AppMainContainer.vue
@@ -0,0 +1,24 @@
+<script>
+export default{
+    name: 'AppMainContainer',
+
+}
+
+</script>
+<template>
+<section>
+    
+</section>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/mixin.scss' as *;
+
+section{
+    @include bgImg('/shoes.jpg');
+    height: 500px;
+    background-color: red;
+
+}
+</style>
